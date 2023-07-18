4d60aea2515c00f09791cb8ef3b1decce8f8af6b
Felipe Bomfim <felipebomfim4161@gmail.com>
2023-07-18 17:49:01 -0300

    Included all sanity schemas necessary for the project

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -1,1 +1,10 @@
-export const schemaTypes = []
+import abouts from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+// import testimonials from './testimonials'
+import workExperience from './workExperience'
+import projects from './projects'
+
+export const schemaTypes = [abouts, brands, contact, experiences, skills, workExperience, projects]
